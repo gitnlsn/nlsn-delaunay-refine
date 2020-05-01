5897b4c083b26a77fec641ba8e4c16f9d5f216a3
nelson kenzo tamashiro <nelsonkenzotamashiro@gmail.com>
2020-05-01 16:21:28 -0300

    Revision before publishing

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,18 @@
+/* Elementary data models */
 pub mod vertex;
+pub mod edge;
+pub mod triangle;
+
+/* Behaviour exported implementation */
 mod distance;
 mod area;
 mod circumcenter;
 mod orientation;
 mod continence;
 mod encroachment;
-pub mod edge;
-pub mod triangle;
+
+/* Data structure that resumes lib main output */
 pub mod triangulation;
 
+/* Triangulation algorithm and utilities */
 pub mod triangulator;
-
